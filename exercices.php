18641ac79c26999f17a14462797a83ed36507f86
DRAGONET-PC\tomva <tom.vallee37@gmail.com>
2017-05-18 13:57:30 +0200

    Fin exercices.php (achanger lors de l'implémentation)

--- exercices.php
+++ exercices.php
@@ -22,8 +22,32 @@ else
         <?php require_once "includes/header.php";  
         AfficherTop($theme);
         Affichertop(0);?>
-       
         
+       <ul class="nav nav-tabs">
+           <?php
+           $query = "SELECT * FROM theme Order By theme_id";
+           $table_theme= getDB()->query($query);
+           while ($tuple = $table_theme->fetch()) {
+               if (isset($_GET['theme']) && $tuple['theme_id'] == $_GET['theme']) {
+                   echo'<li role="presentation" class="active"><a href="exercices.php?theme='.$tuple['theme_id'].'">'.$tuple['theme_titre'].'</a></li>';
+                   } 
+                   else {
+                       echo'<li role="presentation"><a href="exercices.php?theme='.$tuple['theme_id'].'">'.$tuple['theme_titre'].'</a></li>';                   
+                       }
+                   }
+             ?>
+        </ul>
+        <?php
+        $query = "SELECT * FROM feuille Where theme_id=?";                
+        $prepQuery=getDB()->prepare($query);    
+        $prepQuery->execute(array($theme));    
+        $feuilles=$prepQuery->fetch();
+            foreach($prepQuery as $id){
+               echo'<div>';
+               echo$id['feuille_titre'];
+               echo'</div>';
+               }
+        ?>
     </div>
     </body>
         
